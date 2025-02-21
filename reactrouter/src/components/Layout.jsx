1b5638bb944312b8d5dad00d6577f33fe76b0bcd
vaibhav112234 <vaibhav21102001@gmail.com>
2025-02-21 13:17:49 +0530

    react-router-dom

---
+++ reactrouter/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header/Header.jsx"
+import Footer from "./footer/Footer.jsx"
+import { Outlet } from "react-router-dom"
+
+
+function Layout() {
+  return (
+    <>
+      <Header/>
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
